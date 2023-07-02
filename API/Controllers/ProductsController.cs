1e20a612c5446feeef48a713ef9e2dc49a225136
Juan Marino <juan.marino@fedex.com>
2023-07-02 14:08:38 -0500

    continued creating API. Made them async and create core and infrastructure folders

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -6,17 +9,27 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController: ControllerBase
     {
-        //endpoints
-        [HttpGet]
-        public string GetProducts() //remember the access pont will be https://localhost:5001/api/products
+        //constructor
+        private readonly StoreContext _context; //same as this.context
+        public ProductsController(StoreContext context)
         {
-            return "this will be a list of products";
+            _context = context;
+            
+        }
+
+        //endpoints
+        // task helps the api to handle multiple requests at the same time
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() //remember the access pont will be https://localhost:5001/api/products
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
         }
 
         [HttpGet("{id}")] //example passing a parameter. This is one level deeper https://localhost:5001/api/products/2
-        public string GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return "this will be a products";
+            return await _context.Products.FindAsync(id);
         }
     }
 }
