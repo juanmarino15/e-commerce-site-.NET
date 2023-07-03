3b86293d1dafaa94d5eec780642bda6ee185a52b
Juan Marino <juan.marino@fedex.com>
2023-07-03 09:08:21 -0500

    finsihed setting up the data from json file and new routes

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -7,5 +7,7 @@ namespace Core.Interfaces
         //async methods. T
         Task<Product> GetProductByIdAsync(int id);
         Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
     }
 }
