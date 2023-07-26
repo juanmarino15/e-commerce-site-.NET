b2a2891e5bb4b6fbe2e5f05185a4f4bb02446a2e
Juan Marino <juan.marino@fedex.com>
2023-07-26 07:44:53 -0500

    added pagination and product interfaces. Rendered data from the products api

--- client/src/app/nav-bar/nav-bar.component.ts
+++ client/src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+// import { AccountService } from 'src/app/account/account.service';
+// import { BasketService } from 'src/app/basket/basket.service';
+// import { BasketItem } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
+  constructor() // public accountService: AccountService // public basketService: BasketService,
+  {}
 
+  getCount(items: any[]) {
+    // return items.reduce((sum, item) => sum + item.quantity, 0);
+  }
 }
